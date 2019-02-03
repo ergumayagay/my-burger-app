cbae4949d463df063d0b07a35f70d478173ab771
Emilio <emilio.gumayagay@gmail.com>
2019-02-04 00:25:29 +0800

    Made creation of burger dynamic

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,51 @@
 import React from "react";
 
-const burger = (props) =>(
+import classes from './Burger.css';
+import BurgerIngridient from './BurgerIngridient/BurgerIngridient';
 
 
-);
+const burger = (props) =>{
 
+    // console.log("ingridients: " +props.ingridients)
+
+    // let keys = Object.keys(props.ingridients);
+
+    // console.log("keys: "+ keys);
+
+    // let arr = keys.map(igKey =>{
+    //     return [...Array(props.ingridient)]
+    //  }
+    // );
+    
+    // console.log(arr);
+
+    //  let arr2 = arr.map((_,i)=>{
+    //     console.log(i);
+    //  });
+
+    let transformedIngridients = Object.keys(props.ingridients)//returns an array of an object's own property name
+                                            .map(igKey=>{
+                                            //  console.log("key: "+ igKey);
+                                            console.log(igKey+" "+props.ingridients[igKey] );
+                                                return [...Array(props.ingridients[igKey])] //create
+                                                    .map((_,i)=>{
+                                                    //console.log("igkey "+igKey+i+" i: "+i);
+                                                        return <BurgerIngridient key={igKey+i} type={igKey} />
+                                                    });
+                                            }).reduce((arr,el)=>{
+                                                return arr.concat(el);
+                                            },[]);
+
+    if (transformedIngridients.length === 0){
+        transformedIngridients = <p>Please start adding ingridients</p>
+    }
+    
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngridient type="bread-top" />
+            {transformedIngridients}
+            <BurgerIngridient type="bread-bottom" />
+        </div>
+    );
+}
 export default burger;

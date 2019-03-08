44028ed0dddaf634309cfe4f369df79591f2a413
Emilio Gumayagay <emiliogumayagay@gmail.com>
2019-03-08 14:32:42 +0800

    Finished Lesson 161 - 167
    - Creating the order summary modal
    - Showing and hiding the modal
    - implementing the backgdrop
    - adding a custom button component
    - implementing the button component
    - adding the price to the order summary
    - adding a toolbar

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import Auxillary from '../../hoc/Auxillary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
-     salad: 0.5,
-     cheese: 0.4,
-     meat: 1.3,
-     bacon: 0.7
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
 }
 
 class BurgerBuilder extends Component {
@@ -16,7 +18,7 @@ class BurgerBuilder extends Component {
     // constructor(props){
     //     super(props);
     //     this.state ={
-            
+
     //     }
     // }
 
@@ -29,74 +31,94 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4,
         purchaseable: false,
+        purchasing: false,
     }
 
-    updatePurchaseState(ingredients){
+    updatePurchaseState(ingredients) {
 
         const sum = Object.keys(ingredients)
-            .map(igKey =>{
+            .map(igKey => {
                 return ingredients[igKey];
-            }).reduce((sum,element) =>{
+            }).reduce((sum, element) => {
                 return sum + element;
-            },0);
+            }, 0);
 
-        this.setState({purchaseable: sum > 0});
-        console.log(this.state.purchaseable >0);
+        this.setState({ purchaseable: sum > 0 });
+        console.log(this.state.purchaseable > 0);
     }
 
-    addIngredientHandler = (type) =>{
+    addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
         const updatedCount = oldCount + 1;
 
-        const updatedIngredients = {...this.state.ingredients};
+        const updatedIngredients = { ...this.state.ingredients };
         updatedIngredients[type] = updatedCount;
         const priceAddition = INGREDIENT_PRICES[type];
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({
-            totalPrice: newPrice, 
+            totalPrice: newPrice,
             ingredients: updatedIngredients
         });
         this.updatePurchaseState(updatedIngredients);
     }
 
-    removeIngredientHandler = (type) =>{
+    removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
         const updatedCount = oldCount - 1;
-        if(oldCount <= 0){
+        if (oldCount <= 0) {
             return;
         }
-        const updatedIngredients = {...this.state.ingredients};
+        const updatedIngredients = { ...this.state.ingredients };
         updatedIngredients[type] = updatedCount;
         const priceDeduction = INGREDIENT_PRICES[type];
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
         this.setState({
-            totalPrice: newPrice, 
+            totalPrice: newPrice,
             ingredients: updatedIngredients
         });
         this.updatePurchaseState(updatedIngredients);
     }
 
+    purchaseHandler = () =>{
+        this.setState({purchasing:true});
+    }
+
+    purchaseCancelHandler = () =>{
+        this.setState({purchasing:false});
+    }
+
+    purchaseContinueHandler = () =>{
+        alert('You continue');
+    }
+
     render() {
-        
+
         const disabledInfo = {
             ...this.state.ingredients
         };
 
-        for(let key in disabledInfo){
-            disabledInfo[key] = disabledInfo[key] <=0;
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
         }
-        
+
         return (
             <Auxillary>
-                <Burger ingredients = {this.state.ingredients} />
-                <BuildControls 
-                    ingredientAdded = {this.addIngredientHandler}
-                    ingredientRemoved = {this.removeIngredientHandler}
-                    disabled = {disabledInfo}
-                    purchaseable = {this.state.purchaseable}
-                    price = {this.state.totalPrice}
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                    <OrderSummary ingredients={this.state.ingredients} 
+                    purchaseCanceled={this.purchaseCancelHandler}
+                    purchaseContinued={this.purchaseContinueHandler} 
+                    price ={this.state.totalPrice}/>
+                </Modal>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
+                    purchaseable={this.state.purchaseable}
+                    price={this.state.totalPrice}
+                    ordered ={this.purchaseHandler}
                 />
 
             </Auxillary>
